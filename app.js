7b1c1672985f3af61a4c1db3238b8c623333cade
Jblack262 <JBlack262@west-mec.org>
2022-01-25 14:05:13 -0700

    created node template

---
+++ app.js
@@ -0,0 +1,29 @@
+//important things
+const express = require('express')
+const app = express()
+
+//important packages
+require('dotenv').config()
+
+//middleware functions
+app.use(express.json())
+app.use(express.static("./public"));
+
+
+
+const port = process.env.PORT || 3000 ;
+
+//when adding DB functionality comment this out and uncomment the start function
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}....`)
+})
+
+//uncomment this when adding DB functionality
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGO_URI);
+//         // await populateProducts()
+//         app.listen(port, console.log(`server is listening on port ${port}`));
+//     } catch (error) { console.log(error) }
+// }
+// start()
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Navbar = () => {
-  return <div>
-      <h1>This is a cool Navbar</h1>
-  </div>;
-};
-
-export default Navbar;
