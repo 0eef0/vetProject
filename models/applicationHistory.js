16ffdcaf8cd713aef9bb3b0361977032a7249cba
TheHandMaster <69435603+Hshipp267@users.noreply.github.com>
2022-01-26 14:54:00 -0700

    Made an schema of AppHistory

---
+++ models/applicationHistory.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const ApplicationsHistorySchema = new mongoose.Schema({
+    FirstName: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    LastName: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    Email: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    PhoneNumber: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    Age: {
+        type: Number,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    lifeStyles: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    HasPets: {
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    PetHistory: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    Comfort: {
+        type: String,
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    Accepted: {
+        required: [true, 'Must provide a name'],
+        trim: true
+    },
+    Rejected: {
+        required: [true, 'Must provide a name'],
+        trim: true
+    }
+});
+
+// This is basic validation not advanced
+module.exports = mongoose.model('model', ApplicationsHistorySchema);
