b3b277e7611cdcd36ed39fca76f508001a518856
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2022-01-26 15:30:54 -0700

    finished base styling for about us, put sample text in about us, started styling and added template to records

---
+++ public/js/aboutUs.js
@@ -0,0 +1,45 @@
+const main = document.querySelector('#aboutUs main');
+
+async function getInfoAboutUs() {
+    try {
+        // const { data: aboutUs, } = await axios.get(``);
+        main.innerHTML = `
+            <h1>About Us</h1>
+            <div id="infoAboutUs">
+                <p>${'data.main.info1'}Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.30%</p>
+                <img src=${'data.main.img'}/>
+                <p>${'data.main.info2'}Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.</p>
+            </div>
+            <hr/>
+            <h1>Our Teachers/Team</h1>
+            <div class="rows">
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.</div>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.</div>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.</div>
+                <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quos soluta, accusamus culpa, sunt sit
+                mollitia laborum, atque aut recusandae assumenda perferendis nisi odio itaque impedit odit perspiciatis
+                ipsa hic libero quis incidunt? Beatae earum quaerat dignissimos totam accusantium, quis iure at sequi
+                tempora, accusamus vitae ex laboriosam! Minima, mollitia.</div>
+            </div>
+                `;
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+getInfoAboutUs()
