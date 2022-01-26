7b2dad5bf611faa1105b90a4283d48357ed5f2d8
Amakris12 <amakri386@west-mec.org>
2022-01-26 13:27:45 -0700

    Routes and controllers up to date

---
+++ routes/route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAll,create,get,update,deleteIt} = require('../controllers/controller')
+
+router.route('/').get(getAll).post(create);
+router.route('/:id').get(get).patch(update).delete(deleteIt);
+
+module.exports = router;
