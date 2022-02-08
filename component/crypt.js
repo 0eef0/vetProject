71fd8816de86fd2cfd86353084b164adf0f283c0
Amakris12 <amakri386@west-mec.org>
2022-02-08 14:50:14 -0700

    stuf stuf

--- component/crypt.js
+++ component/crypt.js
@@ -22,4 +22,21 @@ const decrypt = (hash) => {
 module.exports = {
     encrypt,
     decrypt
+<<<<<<< Updated upstream
 };
+=======
+};
+const { encrypt, decrypt } = require('./crypto');
+const hash = encrypt('Hello World!');
+console.log(hash);
+
+// {
+//     iv: '237f306841bd23a418878792252ff6c8',
+//     content: 'e2da5c6073dd978991d8c7cd'
+// }
+
+const text = decrypt(hash);
+// console.log(text); // Hello World!
+console.log(text); // Hello World
+
+>>>>>>> Stashed changes
