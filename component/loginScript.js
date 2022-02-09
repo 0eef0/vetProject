f818dec4996d89901561daa432178934ab298f9f
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-02-09 13:02:39 -0700

    Worked on Login

--- component/loginScript.js
+++ component/loginScript.js
@@ -2,10 +2,18 @@ const { encrypt, decrypt } = require('./crypt');
 const hash = encrypt('Hello World!');
 console.log(hash);
 
+const loginFunc = (user, pass) => {
+    console.log("It worked");
+    console.log(user, pass);
+}
+
+
 // {
 //     iv: '237f306841bd23a418878792252ff6c8',
 //     content: 'e2da5c6073dd978991d8c7cd'
 // }
 
-const text = decrypt(hash);
-console.log(text); // Hello World!
+// const text = decrypt(hash);
+// console.log(text); // Hello World!
+
+export default loginFunc;
