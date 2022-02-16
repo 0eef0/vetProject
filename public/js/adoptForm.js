9903b723e0d85f8d5f6ffc83719ff2cac50b42b1
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2022-02-16 13:25:48 -0700

    fixed adoptform.js merge errors

--- public/js/adoptForm.js
+++ public/js/adoptForm.js
@@ -1,4 +1,4 @@
-const adoptFormDOM = document.querySelector('.adoptForm');
+const adoptFormDOM = document.querySelector('.newPetForm');
 const otherHomeInput = document.getElementById('otherHomeRadio');
 
 otherHomeInput.addEventListener('click', () => {
@@ -9,57 +9,73 @@ otherHomeInput.addEventListener('click', () => {
     }
 });
 
-adoptFormDOM.addEventListener('submit', (e) => {
+adoptFormDOM.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    let fullName = document.getElementById('userFullName').value;
-    let occupation = document.getElementById('userOccupation').value;
-    let address = document.getElementById('userAddress').value;
-    let phoneNumber = document.getElementById('userPhoneNumber').value;
-    let email = document.getElementById('userEmail').value;
-    let userReference = document.getElementById('userReference').value;
-    let numChildren = document.getElementById('numChildren').value;
-    let homeType = (document.getElementById('House').checked) ? 'House' : (document.getElementById('Apartment').checked) ? 'Apartment' : (document.getElementById('Condominium').checked) ? 'Condominium' : document.getElementById('Other');
-    let dogSpace = (document.getElementById('lgDogYes').checked) ? true : (document.getElementById('lgDogNo')) ? false : undefined;
-    let minor = (document.getElementById('ifUnder18Yes')) ? true : (document.getElementById('ifUnder18No')) ? false : undefined;
-    let guardianName = document.getElementById('guardianName').value;
-    let guardianNumber = document.getElementById('guardianNumber').value;
-    let guaranteedEmail = document.getElementById('guardianEmail').value;
-    let currentPets = document.getElementById('homePets').value;
-    let currPetSterNVax = (document.getElementById('otherPetSterNVaxYes').checked) ? true : (document.getElementById('otherPetSterNVaxNo').checked) ? false : (document.getElementById('otherPetSterNVaxNA').checked) ? 'N/A' : undefined;
-    let sterNVaxExplain = document.getElementById('sterNVaxExplain').value;
-    let annualExam = (document.getElementById('annualExamYes').checked) ? true : (document.getElementById('annualExamNo').checked) ? false : (document.getElementById('annualExamNA').checked) ? 'N/A' : undefined;
-    let annualExamExplain = document.getElementById('annualExamExplain').value;
-    let bestCandidateExplain = document.getElementById('bestCandidateExplain').value;
-    let petTimeSpent = document.getElementById('petTimeSpent').value;
-    let financial = (document.getElementById('financialYes').checked) ? 'Yes' : (document.getElementById('financialNo').checked) ? 'No' : (document.getElementById('financialMaybe').checked) ? 'Maybe' : 'Undefined';
-    let declaw = (document.getElementById('declawYes')) ? 'Yes' : (document.getElementById('declawNo')) ? 'No' : (document.getElementById('declawNA').checked) ? 'N/A' : undefined;
-    let acknowledgement = document.getElementById('acknowledgement').checked;
-    let acknowledgement2 = document.getElementById('acknowledgement2').checked;
-
+    const petApplication = {
+        fullname: document.getElementById('userFullName').value,
+        occupation: document.getElementById('userOccupation').value,
+        address: document.getElementById('userAddress').value,
+        phoneNumber: document.getElementById('userPhoneNumber').value,
+        email: document.getElementById('userEmail').value,
+        userReference: document.getElementById('userReference').value,
+        children: document.getElementById('numChildren').value,
+        housing: (document.getElementById('House').checked) ? 'House' : (document.getElementById('Apartment').checked) ? 'Apartment' : (document.getElementById('Condominium').checked) ? 'Condominium' : document.getElementById('Other'),
+        userReference: document.getElementById('userReference').value,
+        children: document.getElementById('numChildren').value,
+        housing: (document.getElementById('House').checked) ? 'House' : (document.getElementById('Apartment').checked) ? 'Apartment' : (document.getElementById('Condominium').checked) ? 'Condominium' : document.getElementById('Other'),
+        space: (document.getElementById('lgDogYes').checked) ? true : (document.getElementById('lgDogNo')) ? false : undefined,
+        minor: (document.getElementById('ifUnder18Yes')) ? true : (document.getElementById('ifUnder18No')) ? false : undefined,
+        guardianName: document.getElementById('guardianName').value,
+        guardianPhone: document.getElementById('guardianNumber').value,
+        guaranteedEmail: document.getElementById('guardianEmail').value,
+        currentPets: document.getElementById('homePets').value,
+        petVaccination: (document.getElementById('otherPetSterNVaxYes').checked) ? true : (document.getElementById('otherPetSterNVaxNo').checked) ? false : (document.getElementById('otherPetSterNVaxNA').checked) ? 'N/A' : undefined,
+        petVaccinationReason: document.getElementById('sterNVaxExplain').value,
+        petExamine: (document.getElementById('annualExamYes').checked) ? true : (document.getElementById('annualExamNo').checked) ? false : (document.getElementById('annualExamNA').checked) ? 'N/A' : undefined,
+        petExamineReason: document.getElementById('annualExamExplain').value,
+        qualification: document.getElementById('bestCandidateExplain').value,
+        petTime: document.getElementById('petTimeSpent').value,
+        affordableMedication: (document.getElementById('financialYes').checked) ? 'Yes' : (document.getElementById('financialNo').checked) ? 'No' : (document.getElementById('financialMaybe').checked) ? 'Maybe' : 'Undefined',
+        declaw: (document.getElementById('declawYes')) ? 'Yes' : (document.getElementById('declawNo')) ? 'No' : (document.getElementById('declawNA').checked) ? 'N/A' : undefined,
+        acknowledgement: document.getElementById('acknowledgement').checked,
+        acknowledgementAdoption: document.getElementById('acknowledgement2').checked,
+        // wantedPet: document.getElementById('').value
+    }
+    const appliJSON = JSON.stringify(petApplication);
 
     console.log( {
-        FullName: fullName,
-        Occupation: occupation,
-        Address: address,
-        PhoneNumber: phoneNumber,
-        Email: email,
-        UserReference: userReference,
-        Children: numChildren,
-        Housing: homeType,
-        Space: dogSpace,
-        Minor: minor,
-        GuardianName: guardianName,
-        GuardianPhone: guardianNumber,
-        GuardianEmail: guaranteedEmail,
-        CurrentPets: currentPets,
-        PetExamine: currPetSterNVax,
-        PetExamineReason: sterNVaxExplain,
-        Qualification: bestCandidateExplain,
-        PetTime: petTimeSpent,
-        AffordableMedication: financial,
-        Declaw: declaw,
-        Acknowledgement: acknowledgement,
-        AcknowledgementAdoption: acknowledgement2,
+        FullName: petApplication.fullname,
+        Occupation: petApplication.occupation,
+        Address: petApplication.address,
+        PhoneNumber: petApplication.phoneNumber,
+        Email: petApplication.email,
+        UserReference: petApplication.userReference,
+        Children: petApplication.children,
+        Housing: petApplication.housing,
+        Space: petApplication.space,
+        Minor: petApplication.minor,
+        GuardianName: petApplication.guardianName,
+        GuardianPhone: petApplication.guardianPhone,
+        GuardianEmail: petApplication.guaranteedEmail,
+        CurrentPets: petApplication.currentPets,
+        PetVaccination: petApplication.petVaccination,
+        PetVaccinationReason: petApplication.petVaccinationReason,
+        PetExamine: petApplication.petExamine,
+        PetExamineReason: petApplication.petExamineReason,
+        Qualification: petApplication.qualification,
+        PetTime: petApplication.petTime,
+        AffordableMedication: petApplication.affordableMedication,
+        Declaw: petApplication.declaw,
+        Acknowledgement: petApplication.acknowledgement,
+        AcknowledgementAdoption: petApplication.acknowledgementAdoption,
+        // WantedPet:wantedPet
     });
+
+    try{
+        await axios.post('/api/v1/applications', {appliJSON});
+        console.log(phoneNumber)
+    }catch(error){
+        console.log(error.response.data)
+    }
 })
