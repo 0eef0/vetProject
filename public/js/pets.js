b119ca1be91372c0ded6e0c52c8cb043a30a8be0
Jblack262 <JBlack262@west-mec.org>
2022-02-22 13:03:10 -0700

    started filter functionality

--- public/js/pets.js
+++ public/js/pets.js
@@ -1,6 +1,18 @@
 const petCardContainerDOM = document.querySelector('.card-container');
 
+const filterAllBtn = document.querySelector('#all');
+const filterCatsBtn = document.querySelector('#cats');
+const filterDogsBtn = document.querySelector('#dogs');
+
+const sortByBtn = document.querySelector('.sort-by-container')
+const sortByElem = document.querySelector('.sort-by-value')
+
+const filters = ['Age', 'Date Added', 'A-Z', 'Z-A']
+
+var filterPetSelection = 'all';
+
 const url = "/api/v1/pets";
+
 
 const showPets = async () => {
     try {
@@ -31,3 +43,33 @@ const showPets = async () => {
     }
 }
 showPets()
+
+
+filterAllBtn.addEventListener('click', () => {
+    filterAllBtn.classList.add('active');
+    filterCatsBtn.classList.remove('active');
+    filterDogsBtn.classList.remove('active');
+
+    filterPetSelection = 'all';
+})
+filterCatsBtn.addEventListener('click', () => {
+    filterAllBtn.classList.remove('active');
+    filterCatsBtn.classList.add('active');
+    filterDogsBtn.classList.remove('active');
+
+    filterPetSelection = 'cats';
+})
+filterDogsBtn.addEventListener('click', () => {
+    filterAllBtn.classList.remove('active');
+    filterCatsBtn.classList.remove('active');
+    filterDogsBtn.classList.add('active');
+
+    filterPetSelection = 'dogs';
+})
+
+sortByElem.innerHTML = filters[0];
+sortByBtn.addEventListener('click', () => {
+    nextFilter = filters[filters.indexOf(sortByElem.innerHTML) + 1];
+    if (!nextFilter) nextFilter = filters[0]
+    sortByElem.innerHTML = nextFilter;
+})
