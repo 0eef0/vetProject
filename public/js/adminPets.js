4be2aec27ad7bccfb60a1faa3530a4118116a3f2
Jblack262 <JBlack262@west-mec.org>
2022-02-28 15:14:26 -0700

    filters on admin

--- public/js/adminPets.js
+++ public/js/adminPets.js
@@ -2,6 +2,18 @@ const petCardContainerDOM = document.querySelector('.card-container');
 const newPetFormDOM = document.querySelector('.newPetForm');
 const url = "/api/v1/pets";
 
+const filterAllBtn = document.querySelector('#all');
+const filterCatsBtn = document.querySelector('#cats');
+const filterDogsBtn = document.querySelector('#dogs');
+
+const sortByBtn = document.querySelector('.sort-by-container')
+const sortByElem = document.querySelector('.sort-by-value')
+
+const filters = ['A-Z', 'Z-A', 'Age'];
+let currentFilterIndex = 0;
+
+var filterPetSelection = '';
+
 let imgAmount = 3;
 const addImg = () => {
     if(imgAmount < 12) {
@@ -72,7 +84,7 @@ const showPets = async () => {
             petCardContainerDOM.innerHTML = '<h5 class="empty-list">There are no pets available at this time...</h5>';
              return;
          }
-         const allPets = pets.map((pet) => {
+         const allPets = pets.filter((pet) => filterPetSelection ? (pet.Species == filterPetSelection) : pet).sort((a,b) => sortPets(a,b)).map((pet) => {
             const {_id: id, Name, Birthday, Gender, Medical, Color, Breed, Species, Personality, Notes, IMG} = pet;
             const bDay = new Date(Birthday)
             return `
@@ -95,3 +107,54 @@ const showPets = async () => {
     }
 }
 showPets()
+
+const sortPets = (a,b) => {
+ if (currentFilterIndex == 0) {
+    if(a.Name.toLowerCase() < b.Name.toLowerCase()) { return -1; }
+    if(a.Name.toLowerCase() > b.Name.toLowerCase()) { return 1; }
+    return 0;
+ } else if (currentFilterIndex == 1) {
+    if(a.Name.toLowerCase() < b.Name.toLowerCase()) { return 1; }
+    if(a.Name.toLowerCase() > b.Name.toLowerCase()) { return -1; }
+    return 0;
+ } else {
+    const date1 = new Date(a.Birthday);
+    const date2 = new Date(b.Birthday);
+    return date1 - date2
+ }
+}
+
+filterAllBtn.addEventListener('click', () => {
+    filterAllBtn.classList.add('active');
+    filterCatsBtn.classList.remove('active');
+    filterDogsBtn.classList.remove('active');
+
+    filterPetSelection = '';
+    showPets();
+})
+filterCatsBtn.addEventListener('click', () => {
+    filterAllBtn.classList.remove('active');
+    filterCatsBtn.classList.add('active');
+    filterDogsBtn.classList.remove('active');
+
+    filterPetSelection = 'Cat';
+    showPets();
+})
+filterDogsBtn.addEventListener('click', () => {
+    filterAllBtn.classList.remove('active');
+    filterCatsBtn.classList.remove('active');
+    filterDogsBtn.classList.add('active');
+
+    filterPetSelection = 'Dog';
+    showPets();
+})
+
+sortByElem.innerHTML = filters[currentFilterIndex];
+sortByBtn.addEventListener('click', () => {
+
+    currentFilterIndex < filters.length - 1 ? currentFilterIndex += 1 : currentFilterIndex = 0;
+    
+    sortByElem.innerHTML = filters[currentFilterIndex];
+
+    showPets();
+})
