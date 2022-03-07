ee5146a39a8b4ff71e71b9319aed6cebab9f8d3c
TheCapn-MEC <erolda266@west-mec.org>
2022-03-07 12:09:42 -0700

    made a display for users in the admin account creation page, can delete accoutns, need to hook up to firebase

--- public/js/adminCreate.js
+++ public/js/adminCreate.js
@@ -1,4 +1,5 @@
 const newUserFormDOM = document.querySelector('.newUserForm');
+const currUsersDOM = document.querySelector('#currUsers tbody');
 
 newUserFormDOM.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -8,7 +9,30 @@ newUserFormDOM.addEventListener('submit', async (e) => {
     const email = document.getElementById('email').value;
     const pass = document.getElementById('pass').value;
 
-    console.log({
+    const user = {
         name,username,email,pass
-    })
-})
+    }
+    addUser(user);
+    newUserFormDOM.reset();
+});
+
+const addUser = (user) => {
+    currUsersDOM.innerHTML += `<tr user="${user.email}">
+                    <td class="userInfo">${user.name}</td>
+                    <td class="userInfo">${user.username}</td>
+                    <td class="userInfo">${user.email}</td>
+                    <td class="password userInfo">${user.pass}</td>
+                    <td class="btnPanel">
+                        <button onclick="deleteUser('${user.email}')">Delete User</button>
+                    </td>
+                </tr>`;
+};
+
+const deleteUser = (email) => {
+    for(let i = 0; i < currUsersDOM.children.length; i++) {
+        if (email === currUsersDOM.children[i].attributes.user.nodeValue) {
+            currUsersDOM.children[i].remove()
+            break;
+        }
+    }
+};
