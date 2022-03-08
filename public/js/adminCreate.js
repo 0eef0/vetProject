35c6764dbbc5f9bd38355af08ca066c738c6fd8f
TheCapn-MEC <erolda266@west-mec.org>
2022-03-08 15:20:13 -0700

    worked on admin create page

--- public/js/adminCreate.js
+++ public/js/adminCreate.js
@@ -1,31 +1,29 @@
 const newUserFormDOM = document.querySelector('.newUserForm');
-const currUsersDOM = document.querySelector('#currUsers tbody');
+const currUsersDOM = document.querySelector('#currUsers');
 
 newUserFormDOM.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const name = document.getElementById('name').value;
-    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const pass = document.getElementById('pass').value;
 
     const user = {
-        name,username,email,pass
+        name,email,pass
     }
     addUser(user);
     newUserFormDOM.reset();
 });
 
 const addUser = (user) => {
-    currUsersDOM.innerHTML += `<tr user="${user.email}">
-                    <td class="userInfo">${user.name}</td>
-                    <td class="userInfo">${user.username}</td>
-                    <td class="userInfo">${user.email}</td>
-                    <td class="password userInfo">${user.pass}</td>
-                    <td class="btnPanel">
-                        <button onclick="deleteUser('${user.email}')">Delete User</button>
-                    </td>
-                </tr>`;
+    currUsersDOM.innerHTML += `<div class="user" user="${user.email}">
+                    <p class="name">${user.name}</p>
+                    <p class="email">${user.email}</p>
+                    <p class="password">${user.pass}</p>
+                    <p class="btnPanel">
+                        <button onclick="deleteUser('${user.email}')">Delete ${user.name}</button>
+                    </p>
+                </div>`;
 };
 
 const deleteUser = (email) => {
