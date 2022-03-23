b287fc56efb89ab67af3b5e91d3887cfcf2902cf
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-03-23 14:04:17 -0700

    Admin default and uncommented stuff
    Made status admin by default and uncommented controller functionality

--- controllers/LoginControllers.js
+++ controllers/LoginControllers.js
@@ -6,12 +6,12 @@ const accountModel = require('../models/users');
 // const crypto = ('crypto')
 // const secret = 'abcdefg';
 
-// const createNewAccount = async (req, res) => {
-//     try {
-//         const Login = await accountModel.create(req.body);
-//         res.status(201).json({Login});
-//     } catch (error) { res.status(500).json({msg: error}) }
-// }
+const createNewAccount = async (req, res) => {
+    try {
+        const Login = await accountModel.create(req.body);
+        res.status(201).json({Login});
+    } catch (error) { res.status(500).json({msg: error}) }
+}
 const getAllAccounts = async (req, res) => {
     try {
         const Login = await accountModel.find({});
@@ -24,25 +24,25 @@ const getOneAccount = async (req, res) => {
         res.status(201).json({ Login });
     } catch (error) { res.status(500).json({ msg: error }) }
 }
-// const deleteAccount = async (req, res) => {
-//     try {
-//         const Login = await accountModel.findByIdAndRemove(req.params.id);
-//         res.status(201).json({Login});
-//     } catch (error) { res.status(500).json({msg: error}) }
-// }
-// const updateAccount = async (req, res) => {
-//     try {
-//         const {id} = req.params;
-//         const newLogin = req.body;
-//         const Login = await accountModel.findOneAndUpdate({ _id: id }, newLogin);
-//         res.status(201).json({Login});
-//     } catch (error) { res.status(500).json({msg: error}) }
-// }
+const deleteAccount = async (req, res) => {
+    try {
+        const Login = await accountModel.findByIdAndRemove(req.params.id);
+        res.status(201).json({Login});
+    } catch (error) { res.status(500).json({msg: error}) }
+}
+const updateAccount = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const newLogin = req.body;
+        const Login = await accountModel.findOneAndUpdate({ _id: id }, newLogin);
+        res.status(201).json({Login});
+    } catch (error) { res.status(500).json({msg: error}) }
+}
 
 module.exports = {
-    // createNewAccount,
+    createNewAccount,
     getAllAccounts,
-    getOneAccount
-    // deleteAccount,
-    // updateAccount,
+    getOneAccount,
+    deleteAccount,
+    updateAccount
 }
